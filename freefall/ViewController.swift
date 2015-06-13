c084b3058f5d6d3d465e6da07544357ca8882416
shinokiryosei <shinoki.r.0301@gmail.com>
2015-06-13 10:53:41 +0800

    first commit

--- freefall/ViewController.swift
+++ freefall/ViewController.swift
@@ -10,14 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//    @IBOutlet weak var ball: UIView!
+    
+    @IBOutlet var balls: [UIView]!
+    
+//    DynamicAnimatorを変数宣言する
+    var dynamicAnimator: UIDynamicAnimator!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.startGame()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func startGame() {
+//        UIDynamicAnimatorのクラス群のUIDynamicBehaviorを使う
+//        let gravityBehavior = UIGravityBehavior(items: [self.ball])
+        let gravityBehavior = UIGravityBehavior(items: self.balls)
+        
+        dynamicAnimator = UIDynamicAnimator(referenceView: self.view)
+        dynamicAnimator.addBehavior(gravityBehavior)
+        
+//        衝突のシュミレーションのためにUICollisionBehaviorを使う
+//        let collisionBehavior = UICollisionBehavior(items: [self.balls])
+        let collisionBehavior = UICollisionBehavior(items: self.balls)
+        
+      
+        
+        
+//        外枠にボールがぶつかれば跳ね返るようにする
+        collisionBehavior.translatesReferenceBoundsIntoBoundary = true
+        
+        
+        dynamicAnimator.addBehavior(collisionBehavior)
+        
+        
+        
     }
 
 
